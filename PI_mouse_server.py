ec62368add5d14f980932c8ab84cc4c415f77166
djblazkowicz <40417349@live.napier.ac.uk>
2023-06-25 21:17:25 +0100

    asd

--- PI_mouse_server.py
+++ PI_mouse_server.py
@@ -28,7 +28,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM); # UDP
 GPIO.setwarnings(False); # Ignore warning for now
 GPIO.setmode(GPIO.BOARD); # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN); # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback); # Setup event on pin 10 rising edge
+GPIO.add_event_detect(10,GPIO.FALLING,callback=button_callback); # Setup event on pin 10 rising edge
 
 while( 1 ):
   r,w,x = select([dev],[],[]);
