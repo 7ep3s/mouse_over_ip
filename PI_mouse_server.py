9a6337caeb7519f94efc202cfa3fe8a2d7e3e8e1
djblazkowicz <40417349@live.napier.ac.uk>
2023-06-25 21:41:02 +0100

    asd

--- PI_mouse_server.py
+++ PI_mouse_server.py
@@ -25,10 +25,10 @@ dev = InputDevice('/dev/input/event1');
 PORT = 50000
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM); # UDP
 
-#GPIO.setwarnings(False); # Ignore warning for now
-#GPIO.setmode(GPIO.BOARD); # Use physical pin numbering
-#GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN); # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-#GPIO.add_event_detect(10,GPIO.FALLING,callback=button_callback); # Setup event on pin 10 rising edge
+GPIO.setwarnings(False); # Ignore warning for now
+GPIO.setmode(GPIO.BOARD); # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN); # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10,GPIO.FALLING,callback=button_callback); # Setup event on pin 10 rising edge
 
 while( 1 ):
   r,w,x = select([dev],[],[]);
@@ -41,4 +41,4 @@ while( 1 ):
     print(message)
     print(IPs[index])
 
-#GPIO.cleanup(); # Clean up
+GPIO.cleanup(); # Clean up
